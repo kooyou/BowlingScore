0d8018f1d2696febf61b9fb4b045c2e83f973a1c
Jo Feng <535826356@qq.com>
2012-04-19 16:39:40 +0800

    first commit

---
+++ BowlingScoring/Game.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingScoring
+{
+    public class Game
+    {
+        private int mv_score;
+        private int[] mv_throws = new int[21];
+        private int mv_currentThrow;
+        private int mv_currentFrame = 1;
+        private bool mv_isFirstThrow = true;
+
+        public int currentFrame
+        {
+            get { return mv_currentFrame; }
+        }
+        public int score
+        {
+            get { return ScoreForFrame(mv_currentFrame); }
+        }
+
+        public int GetCurrentFrame()
+        {
+            return mv_currentFrame;
+        }
+
+        public void Add(int pins)
+        {
+            mv_throws[mv_currentThrow++] = pins;
+            mv_score += pins;
+
+            AdjustCurrentFrame(pins);
+        }
+
+        private void AdjustCurrentFrame(int pins)
+        {
+            if (Strike(pins) || (!mv_isFirstThrow))
+            {
+                AdvanceFrame();
+                mv_isFirstThrow = true;
+            }
+            else
+                mv_isFirstThrow = false;
+            /*
+            if (mv_isFirstThrow)
+            {
+                if(AdjustFrameForStrike(pins)==false)
+                mv_isFirstThrow = false;
+            }
+            else
+            {
+                mv_isFirstThrow = true;
+                AdvanceFrame();
+            }*/
+        }
+
+        private bool AdjustFrameForStrike(int pins)
+        {
+            if (pins == 10)
+            {
+                AdvanceFrame();
+                return true;
+            }
+            return false;
+        }
+
+        private void AdvanceFrame()
+        {
+            mv_currentFrame++;
+            if (mv_currentFrame > 10)
+                mv_currentFrame = 10;
+        }
+
+        private int ball;
+        private int firstThrow;
+        private int secondThrow;
+
+        public int ScoreForFrame(int theFrame)
+        {
+            ball = 0;
+            int score = 0;
+            for (int currentFrame = 0;
+                currentFrame < theFrame;
+                currentFrame++)
+            {
+                firstThrow = mv_throws[ball];
+                if (Strike())
+                {
+                    score += 10 + NextTwoBallsForStrike;
+                    ball++;
+                }
+                else if (Spare())
+                {
+                    score += 10 + NextBallForSpare;
+                    ball += 2;
+                }
+                else
+                {
+                    score += TwoBallsInframe;
+                    ball += 2;//谁先谁后让人很恶心
+                }
+            }
+            return score;
+        }
+
+        private int NextTwoBallsForStrike
+        {
+            get { return (mv_throws[ball+1] + mv_throws[ball + 2]); }
+        }
+        private bool Strike(int pins)
+        {
+            return (mv_isFirstThrow && pins == 10);
+        }
+
+        private bool Strike()
+        {
+            return mv_throws[ball] == 10;
+        }
+
+        private int HandleSecondThrow()
+        {
+            int score=0;
+            secondThrow = mv_throws[ball+1];
+
+            //分瓶时需要知道下一轮的第一投
+            if (Spare())
+            {
+                ball += 2;//谁先谁后让人很恶心
+                score += 10 + NextBallForSpare;
+            }
+            else
+            {
+                score += TwoBallsInframe;
+                ball += 2;//谁先谁后让人很恶心
+            }
+            return score;
+        }
+
+        private int TwoBallsInframe
+        {
+            get { return mv_throws[ball] + mv_throws[ball + 1]; }
+        }
+
+        private int NextBallForSpare
+        {
+            get { return mv_throws[ball+2]; }
+        }
+
+        private bool Spare()
+        {
+            return mv_throws[ball]+mv_throws[ball+1]==10;
+        }
+    }
+}
---
+++ BowlingScoringTest/GameTest.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScoring;
+
+namespace BowlingScoringTest
+{
+    /// <summary>
+    /// UnitTest1 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class GameTest
+    {
+        private Game mv_game;
+        public GameTest()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+            mv_game = new Game();
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，该上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试特性
+        //
+        // 编写测试时，可以使用以下附加特性:
+        //
+        // 在运行类中的第一个测试之前使用 ClassInitialize 运行代码
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在类中的所有测试都已运行之后使用 ClassCleanup 运行代码
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在运行每个测试之前，使用 TestInitialize 来运行代码
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在每个测试运行完之后，使用 TestCleanup 来运行代码
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestFourThrowNoMark()
+        {
+            //
+            // TODO: 在此处添加测试逻辑
+            //
+
+            mv_game.Add(2);
+            mv_game.Add(5);
+            mv_game.Add(6);
+            mv_game.Add(7);
+            Assert.AreEqual(20, mv_game.score);
+            Assert.AreEqual(7, mv_game.ScoreForFrame(1));
+            Assert.AreEqual(20, mv_game.ScoreForFrame(2));
+            Assert.AreEqual(3, mv_game.currentFrame);
+        }
+
+  /*      [TestMethod]
+        public void TestOneThrow()
+        {
+            mv_game.Add(5);
+            Assert.AreEqual(5, mv_game.score);
+            Assert.AreEqual(1, mv_game.currentFrame);
+        }*/
+
+        [TestMethod]
+        public void TestTwoThrowNoMark()
+        {
+            mv_game.Add(4);
+            mv_game.Add(5);
+            Assert.AreEqual(9, mv_game.score);
+            Assert.AreEqual(2, mv_game.currentFrame);
+        }
+
+        [TestMethod]
+        public void TestSimpleFrameAfterSpare()
+        {
+            mv_game.Add(3);
+            mv_game.Add(7);
+            mv_game.Add(3);
+            mv_game.Add(2);
+            Assert.AreEqual(13, mv_game.ScoreForFrame(1));
+            Assert.AreEqual(18, mv_game.ScoreForFrame(2));
+            Assert.AreEqual(18, mv_game.score);
+            Assert.AreEqual(3, mv_game.currentFrame);
+
+        }
+
+        [TestMethod]
+        public void TestSimpleSpare()
+        {
+            mv_game.Add(3);
+            mv_game.Add(7);
+            mv_game.Add(3);
+            Assert.AreEqual(13, mv_game.ScoreForFrame(1));
+            Assert.AreEqual(2, mv_game.currentFrame);
+        }
+
+        [TestMethod]
+        public void TestSimpleStrike()
+        {
+            mv_game.Add(10);
+            mv_game.Add(3);
+            mv_game.Add(6);
+            Assert.AreEqual(19, mv_game.ScoreForFrame(1));
+            Assert.AreEqual(28,mv_game.score);
+            Assert.AreEqual(3, mv_game.currentFrame);
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            for (int i = 0; i < 12; ++i)
+            {
+                mv_game.Add(10);
+            }
+            Assert.AreEqual(300, mv_game.score);
+            Assert.AreEqual(10, mv_game.currentFrame);
+        }
+
+        [TestMethod]
+        public void TestEndOfArray()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                mv_game.Add(0);
+                mv_game.Add(0);
+            }
+            mv_game.Add(2);
+            mv_game.Add(8);
+            mv_game.Add(10);
+            Assert.AreEqual(20, mv_game.score);
+        }
+
+        [TestMethod]
+        public void TestSampleGame()
+        {
+            mv_game.Add(1);
+            mv_game.Add(4);
+            mv_game.Add(4);
+            mv_game.Add(5);
+            mv_game.Add(6);
+            mv_game.Add(4);
+            mv_game.Add(5);
+
+            mv_game.Add(5);
+            mv_game.Add(10);
+            mv_game.Add(0);
+            mv_game.Add(1);
+            mv_game.Add(7);
+            mv_game.Add(3);
+            mv_game.Add(6);
+            mv_game.Add(4);
+            mv_game.Add(10);
+            mv_game.Add(2);
+            mv_game.Add(8);
+            mv_game.Add(6);
+
+            Assert.AreEqual(133, mv_game.score);
+        }
+
+        [TestMethod]
+        public void TestHeartBreak()
+        {
+            for(int i=0;i<11;i++)
+            {
+                mv_game.Add(10);
+            }
+            mv_game.Add(9);
+            Assert.AreEqual(299, mv_game.score);
+        }
+
+        [TestMethod]
+        public void TestTenthFrameSpare()
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                mv_game.Add(10);
+            }
+            mv_game.Add(9);
+            mv_game.Add(1);
+            mv_game.Add(1);
+            Assert.AreEqual(270, mv_game.score);
+        }
+    }
+}
